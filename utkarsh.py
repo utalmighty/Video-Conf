01c6f059fa142b099eeab8514fd1d8c19fc6d431
Utkarsh Jaiswal <utalmighty@gmail.com>
2020-05-19 15:26:03 +0530

    removed debugging print statements

--- utkarsh.py
+++ utkarsh.py
@@ -5,10 +5,10 @@ import os
 #import psycopg2 as postgres
 
 database = []
-connections = []
+
+
 
 con = Flask(__name__)
-
 con.secretKey = os.environ.get('Secret_Key')
 socket = SocketIO(con)
 
@@ -17,7 +17,6 @@ socket = SocketIO(con)
 def errpage(e):
     return render_template('error.html')
 
-print('\n Logs: \n')
 
 @con.route('/about')
 def about():
@@ -43,8 +42,7 @@ def Credentials(credential_json):
             database.append({'username': credential_json['username'],
                             'password': credential_json['password'], 'sessionid': sid})
             emit('flashing', {'messageid':1, 'message':'Added!'}, namespace='/flash')
-            print('Active Clients in database:', len(database))
-            print('\n User: ', credential_json['username'], ', Password:', credential_json['password'], '. Session ID: ', sid, 'Added to database')
+            
             
     else:
         for i in database:
@@ -52,7 +50,6 @@ def Credentials(credential_json):
                 if i['password'] == credential_json['password']:
                     whom = i['sessionid']
                     emit('Credentials', whom, namespace='/private')
-                    print(request.sid ,'<- Meeting ->', whom)
                     break
         else:
             emit('flashing', {'messageid':3, 'message':'No such concurrent Meeting'}, namespace='/flash')
@@ -61,28 +58,15 @@ def Credentials(credential_json):
 def video(image):# image contain whom to send and image data
     emit('videofromflask', image['img'], room = image['to'], namespace='/video')# add room = image['to']
 
-@socket.on('connect', namespace='/private')
-def removeuser():
-    s = request.sid
-    global count
-    count += 1
-    print('Connected', s)
-    
-    print('Active Clients: ', count)
 
 @socket.on('disconnect', namespace='/private')
 def removeuser():
-    global count
-    count -= 1
     rem = request.sid
-    print('Disconnected', rem)
-    print('Active Clients: ', count)
     for i,j in enumerate(database):
         if j['sessionid'] == rem:
             database.remove(database[i])
-            print('Active Clients in database:', len(database))
             break
 
 if __name__ == '__main__':
+    socket.run(con)
     #con.run()
-    con.run()
